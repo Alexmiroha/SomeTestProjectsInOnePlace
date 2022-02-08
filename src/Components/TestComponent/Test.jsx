5de8b681f4a46f4a7aa34d23fb83720b403313f9
Alexmiroha <Alexmiroha@mail.ru>
2022-02-08 20:24:57 +0100

    first commit

---
+++ src/Components/TestComponent/Test.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import s from './Test.module.css'
+
+const Test = () => {
+    return (
+        <div>
+            <div className={s.test}>Hello, it's a test Component</div>
+        </div>
+    );
+};
+
+export default Test;
